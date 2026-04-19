d95340a4242833424e50bd71718807652b6d018a
Mag1c.H <hemajun815@163.com>
2026-04-19 23:27:24 +0800

    新增仿真场景下的内存申请与释放逻辑

---
+++ module/trans/simu/trans_buffer_simu.h
@@ -0,0 +1,87 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2026 Mag1c.H
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+#ifndef TRANS_BUFFER_SIMU_H
+#define TRANS_BUFFER_SIMU_H
+
+#include <cstdlib>
+#include <cstring>
+#include <sys/mman.h>
+#include "trans_buffer.h"
+
+class HostNormalBuffer : public TransBuffer {
+public:
+    HostNormalBuffer(std::size_t device, std::size_t size, std::size_t number)
+        : TransBuffer{device, size, number}
+    {
+        const auto total = size * number;
+        addr_ = malloc(total);
+        std::memset(addr_, 'n', total);
+    }
+    ~HostNormalBuffer() override
+    {
+        if (addr_) { free(addr_); }
+        addr_ = nullptr;
+    }
+    std::string Name() const override { return "host::normal::" + std::to_string(device_); }
+};
+
+class HostAnonymousBuffer : public TransBuffer {
+public:
+    HostAnonymousBuffer(std::size_t device, std::size_t size, std::size_t number)
+        : TransBuffer{device, size, number}
+    {
+        const auto total = size * number;
+        const auto prot = PROT_WRITE | PROT_READ;
+        const auto flags = MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE;
+        addr_ = mmap(nullptr, total, prot, flags, -1, 0);
+        std::memset(addr_, 'a', total);
+    }
+    ~HostAnonymousBuffer() override
+    {
+        const auto total = size_ * number_;
+        if (addr_) { munmap(addr_, total); }
+        addr_ = nullptr;
+    }
+    std::string Name() const override { return "host::anon::" + std::to_string(device_); }
+};
+
+class DeviceNormalBuffer : public TransBuffer {
+public:
+    DeviceNormalBuffer(std::size_t device, std::size_t size, std::size_t number)
+        : TransBuffer{device, size, number}
+    {
+        const std::size_t total = size * number;
+        const std::size_t alignment = 4096;
+        posix_memalign(&addr_, alignment, total);
+        std::memset(addr_, 'p', total);
+    }
+    ~DeviceNormalBuffer() override
+    {
+        if (addr_) { free(addr_); }
+        addr_ = nullptr;
+    }
+    std::string Name() const override { return "device::normal::" + std::to_string(device_); }
+};
+
+#endif
